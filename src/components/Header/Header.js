3f41504fa1c397da19659b218343ad17097c50ba
Eng-Sillah <https://github.com/Eng-Sillah>
2024-02-05 08:38:18 +0000

    Updating Header and Hero section

---
+++ src/components/Header/Header.js
@@ -0,0 +1,79 @@
+// Header.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'; // Import the CSS file
+
+const Header = () => {
+  return (
+    <header className="header-container">
+      <Link to="/" className="logo">
+        Social Work Association Sierra Leone
+      </Link>
+      <nav className="navigation">
+        <div className="top-row">
+          <Link to="/advert" className="nav-item">
+            Advert
+          </Link>
+          <Link to="/donate" className="nav-item">
+            Donate
+          </Link>
+          <Link to="/login" className="nav-item">
+            Login
+          </Link>
+        </div>
+        <div className="row">
+          <div className="nav-dropdown">
+            <Link to="/about" className="nav-item">
+              About
+            </Link>
+            <div className="dropdown-content">
+              <Link to="/governance">Governance</Link>
+              <Link to="/delegate-assembly">Delegate Assembly</Link>
+              <Link to="/career">Career</Link>
+              <Link to="/ethics">Ethics</Link>
+              <Link to="/legal-defence-fund">Legal Defence Fund</Link>
+            </div>
+          </div>
+          <div className="nav-dropdown">
+          <Link to="/practice" className="nav-item">
+            Practice
+          </Link>
+          <div className="dropdown-content">
+              <Link to="/governance">Aging</Link>
+              <Link to="/delegate-assembly">Behavioral Health</Link>
+              <Link to="/career">Child Welfare</Link>
+              <Link to="/ethics">Clinical Social Work</Link>
+              <Link to="/legal-defence-fund">Legal Defence Fund</Link>
+            </div>
+            </div> 
+          <Link to="/career" className="nav-item">
+            Career
+          </Link>
+          <Link to="/advocacy" className="nav-item">
+            Advocacy
+          </Link>
+          <Link to="/news" className="nav-item">
+            News
+          </Link>
+          <Link to="/events" className="nav-item">
+            Events
+          </Link>
+          <div className="nav-dropdown">
+          <Link to="/emembership" className="nav-item">
+            Membership
+          </Link>
+          <div className="dropdown-content">
+              <Link to="/governance">Membership type</Link>
+              <Link to="/delegate-assembly">Professional Benefits</Link>
+              <Link to="/career">Student Benefits</Link>
+              <Link to="/legal-defence-fund">Membership FAQ</Link>
+            </div>
+            </div>
+          {/* Add more regular navigation links as needed */}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/Hero/Hero.js
@@ -0,0 +1,17 @@
+// Hero.js
+import React from 'react';
+import './Hero.css'; // Import the CSS file
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-content">
+        <h1>Welcome to Social Work Association</h1>
+        <p>The independent professional membership organisation for social work across the Sierra Leone</p>
+        <button>Learn More</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
